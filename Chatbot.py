37dd0f0d6a80b6f2b658827b610494db24af6005
root <root@calm-idea-1.localdomain>
2023-11-25 03:37:38 -0500

    Remove key

--- Chatbot.py
+++ Chatbot.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import streamlit as st
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", value="Kxo8iNB9iof3w0pmeS3tT3BlbkFJ54FlFhdi9eNN5msBqi", key="chatbot_api_key", type="password")
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
